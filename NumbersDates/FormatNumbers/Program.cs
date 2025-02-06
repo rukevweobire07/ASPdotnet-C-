e16b821163d0835f219ff0d0700530bf1f4bc001
rukevweobire07 <rukeishere@gmail.com>
2025-02-06 01:05:06 -0800

    format

--- NumbersDates/FormatNumbers/Program.cs
+++ NumbersDates/FormatNumbers/Program.cs
@@ -13,11 +13,18 @@ decimal val2 = 1234.5678m;
 // E (Exponential), D (Decimal), P (Percent), X (Hexadecimal),
 // C (Currency in local format)
 
+Console.WriteLine($"{val1:D}, {val1:N}, {val1:F}");
+Console.WriteLine($"{val2}");
 
 // TODO: Add a number after the format to specify precision
-
+Console.WriteLine($"{val1:D6}, {val1:N2},  {val1:G3}");
 
 // TODO: Formatting with alignment and spacing
+Console.WriteLine("Sales by Quarter:");
+Console.WriteLine($"{quarters[0], 12} {quarters[1], 12} {quarters[2], 12}{quarters[3]}");
+Console.WriteLine($"{sales[0], 12:C0} {sales[1], 12:C0} {sales[2], 12:C0}{sales[3], 12:C0}");
+Console.WriteLine("International Sales:");
+Console.WriteLine($"{intlMixPct[0], 12:P0} {intlMixPct[1], 12:P0} {intlMixPct[2],12:P1} {intlMixPct[3],12:P1}");
 // Console.WriteLine("Sales by Quarter:");
 // Console.WriteLine($"{quarters[0]} {quarters[1]} {quarters[2]} {quarters[3]}");
 // Console.WriteLine($"{sales[0]} {sales[1]} {sales[2]} {sales[3]}");
