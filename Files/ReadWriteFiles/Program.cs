eb5ce0063a1cb531ac593a2f9061733d98d3875e
rukevweobire07 <rukeishere@gmail.com>
2025-02-13 10:45:32 +0100

    ReadWriteFiles

--- Files/ReadWriteFiles/Program.cs
+++ Files/ReadWriteFiles/Program.cs
@@ -6,14 +6,20 @@ const string filename = "TestFile.txt";
 
 // TODO 1: WriteAllText overwrites a file with the given content
 if (!File.Exists(filename)) {
-
+    File.WriteAllText(filename, "This is application programming. ");
 }
 
 // TODO 3: AppendAllText adds text to an existing file
-
+File.AppendAllText(filename, "300 level second semester. ");
 
 // TODO 4: A FileStream can be opened and written to until closed
-
+using(StreamWriter sw = File.AppendText(filename)){
+    sw.WriteLine("Rukevwe");
+    sw.WriteLine("Obire");
+    sw.WriteLine("BU/22C/IT/7476");
+}
 
 // TODO 2: ReadAllText reads all the content from a file
-
+string content;
+content = File.ReadAllText(filename);
+Console.WriteLine(content);
---
+++ NumbersDates/Challenge/Challenge(assignment).cs
@@ -0,0 +1,38 @@
+﻿using System;
+public class Days
+{
+    public static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.WriteLine("Enter a date in the (dd/mm/yyyy) or type 'exit' to quit:");
+            string input = Console.ReadLine();
+            if (input.ToLower() == "exit")
+            {
+                break;
+                }
+                if (DateTime.TryParse(input, out DateTime enteredDate)){
+                DateTime currentDate = DateTime.Now;
+                if (enteredDate > currentDate)
+                {
+                    TimeSpan timeUntil = enteredDate - currentDate;
+                    Console.WriteLine($"There are {timeUntil.Days} days remaining until {enteredDate:dd-MM-yyyy}.");
+                }
+                else if (enteredDate < currentDate)
+                {
+                    TimeSpan timeSince = currentDate - enteredDate;
+                    Console.WriteLine($"There have been {timeSince.Days} days since {enteredDate:dd-MM-yyyy}.");
+                }
+                else
+                {
+                    Console.WriteLine($"The date {enteredDate:dd-MM-yyyy} is today.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("wrong input. try again");
+            }
+        }
+    }
+}
+
