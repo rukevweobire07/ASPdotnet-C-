5dd19b8673ba8333d9b24ac0d49c3b78ceac7b28
rukevweobire07 <rukeishere@gmail.com>
2025-02-13 09:57:21 +0100

    CreateFiles

--- NumbersDates/FormatDates/Program.cs
+++ NumbersDates/FormatDates/Program.cs
@@ -1,28 +1,28 @@
 ﻿// Application Programming .NET Programming with C# by Abdullahi Tijjani
 // Example file for formatting date information
-
+using System.Globalization;
 // Define a date
 DateTime AprFools = new DateTime(2025, 4, 1, 13, 23, 30);
 
 // TODO: 'd' Short date: mm/dd/yyyy (or dd/mm depending on locale)
-
+Console.WriteLine($"{AprFools:d}");
 
 // TODO: 'D' Full date: mm/dd/yyyy (or dd/mm depending on locale)
-
+Console.WriteLine($"{AprFools:D}");
 
 // TODO: 'f' Full date, short time
-
+Console.WriteLine($"{AprFools:f}");
 
 // TODO: 'F' Full date, long time
-
+Console.WriteLine($"{AprFools:F}");
 
 // TODO: 'g' General date and time
-
+Console.WriteLine($"{AprFools:g}");
 
 // TODO: 'G' General date and time
-
+Console.WriteLine($"{AprFools:G}");
 
 // TODO: Format using a specific CultureInfo
-
+Console.WriteLine(AprFools.ToString("d", CultureInfo.CreateSpecificCulture("us-US")));
 
 // TODO: Defining custom date and time formats
